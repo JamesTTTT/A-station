8e52367385f5487dec7f0f6308c561fa38195427
James Taylor <taylormarkaryd@gmail.com>
2025-10-05 22:49:32 +0200

    users model using Django abstract user

---
+++ a-station-django-rest/AStationRest/users/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined', 'workspace_count']
+    list_filter = ['is_staff', 'is_superuser', 'is_active', 'date_joined', 'last_login']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email')
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+            'classes': ('collapse',)
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined'),
+            'classes': ('collapse',)
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+    readonly_fields = ['date_joined', 'last_login']
+
+    def workspace_count(self, obj):
+        return obj.workspaces.count()
+    workspace_count.short_description = 'Workspaces'
