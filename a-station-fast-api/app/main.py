6af884ae83dda45508010a3534b53a5a99cc8ea4
JamesTTTT <taylormarkaryd@gmail.com>
2025-10-11 02:13:20 +0200

    app iniialized

---
+++ a-station-fast-api/app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+# from core.config import settings
+from schemas.health import HealthResponse
+from fastapi.middleware.cors import CORSMiddleware
+# from routes.posts import router
+
+app = FastAPI(
+    title="A-Station",
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.include_router(router=router, prefix="/api")
+
+@app.get("/", response_model=HealthResponse)
+async def health():
+    return HealthResponse(status="Ok")
