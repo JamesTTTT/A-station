7170a872a1d11cc15e85c2a35e62120bf548562b
James Taylor <taylormarkaryd@gmail.com>
2025-10-13 11:58:21 +0200

    config for api_key

--- a-station-fast-api/app/core/config.py
+++ a-station-fast-api/app/core/config.py
@@ -10,12 +10,13 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
 
     # Security
-    SECRET_KEY: str
+    SECRET_KEY: str = ""
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
+    API_KEY_1: str =""
 
     # Database
-    DATABASE_URL: str
+    DATABASE_URL: str = ""
 
     # CORS
     BACKEND_CORS_ORIGINS: list[str] = ["http://localhost:3000", "http://localhost:8000"]
