0543b888a0041e2628bdef920dc876587a97e3fb
James Taylor <taylormarkaryd@gmail.com>
2025-10-20 00:14:17 +0200

    Basic shadcn components

---
+++ a-station-react-app/src/components/ui/index.tsx
@@ -0,0 +1,32 @@
+/**
+ * shadcn/ui Components
+ * Central export for all shadcn/ui components
+ */
+
+export { Button, buttonVariants } from "./button";
+export { Input } from "./input";
+export { Label } from "./label";
+export {
+  Field,
+  FieldLabel,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLegend,
+  FieldSeparator,
+  FieldSet,
+  FieldContent,
+  FieldTitle,
+} from "./field";
+
+export {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuContent,
+  NavigationMenuTrigger,
+  NavigationMenuLink,
+  NavigationMenuIndicator,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "./navigation-menu";
