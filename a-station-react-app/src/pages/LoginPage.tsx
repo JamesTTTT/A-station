51ac9f5bb1d9fa91dad78572ac78c5deb61436d1
jamestaylor <taylormarkaryd@gmail.com>
2025-10-29 21:43:30 +0100

    Updated login page to redirect if successful

--- a-station-react-app/src/pages/LoginPage.tsx
+++ a-station-react-app/src/pages/LoginPage.tsx
@@ -1,10 +1,52 @@
 import { LoginFields } from "@/components";
+import { useState } from "react";
+import { useAuth } from "@/contexts/AuthContext.tsx";
+import { useNavigate } from "@tanstack/react-router";
 
 export const LoginPage = () => {
+  const { login } = useAuth();
+  const navigate = useNavigate({ from: "/login" });
+
+  const [loginFormData, setLoginFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const res = await login(loginFormData);
+
+    if (res.success) {
+      console.log("Logged in");
+      await navigate({
+        to: "/dashboard",
+        replace: true,
+      });
+    } else {
+      console.log(res.error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-      <LoginFields />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginFields
+              loginFormData={loginFormData}
+              setLoginFormData={setLoginFormData}
+              handleSubmit={handleSubmit}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/placeholder.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
     </div>
   );
 };
